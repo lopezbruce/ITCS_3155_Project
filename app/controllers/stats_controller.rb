97302f452ea820f106e331180f588e6beaf9b57f
Bruce <blopez6@uncc.edu>
2017-11-07 02:29:29 +0000

    Chris: Moved Javascript to application.js, added route to index.html.erb for script

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,2 +1,23 @@
 class StatsController < ApplicationController
+    def new
+        @stat = Stat.new
+    end
+    
+    def create
+        @stat = Stat.new(stat_params)
+    end
+=begin
+    implement later
+    
+    def resetHighscore
+        
+    end
+    def editHighscore
+        
+    end
+=end
+    private
+    def stat_params
+        params.require(:stat).permit(:score)
+    end
 end
