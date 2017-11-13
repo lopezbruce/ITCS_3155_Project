5aafdeba552aa9fe962aac0fdde03a21b57bf23a
Bruce <blopez6@uncc.edu>
2017-11-13 21:53:30 +0000

    Add user sign up and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   resources :stats
   
-  get 'layouts/game' => 'welcome#game'
+ get 'layout/game'
+  
+  get '/views/layouts/game' => 'welcome#game'
 
   
   root 'welcome#index'
@@ -10,7 +12,12 @@ Rails.application.routes.draw do
  
  # These routes will be for signup. The first renders a form in the browse, the second will 
     # receive the form and create a user in our database using the data given to us by the user.
-    get '/signup' => 'users#new'
-    post '/users' => 'users#create'
+   # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
 end
