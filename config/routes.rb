cbbfa767b8a88a6e42dffb3bb084b3b75d7f8e39
Bruce <blopez6@uncc.edu>
2017-11-13 21:38:31 +0000

    Fixed a rake db:migation

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  
   resources :stats
+  
+  get '/game', :to => redirect('/game.html')
+
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
+ # These routes will be for signup. The first renders a form in the browse, the second will 
+    # receive the form and create a user in our database using the data given to us by the user.
+    get '/signup' => 'users#new'
+    post '/users' => 'users#create'
+
 end
