5b1cb8809aea44e95952691bcf4fcfb953d63e3a
Bruce <blopez6@uncc.edu>
2017-11-16 02:50:39 +0000

    adding email and password confirmation successful

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171116022540) do
+ActiveRecord::Schema.define(version: 20171116025018) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20171116022540) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "password"
+    t.string "password_confirmation"
+    t.string "email"
   end
 
 end
