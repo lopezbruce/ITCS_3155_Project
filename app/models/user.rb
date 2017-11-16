82e2d85c4e94a4a525e8825f5977c941f2fd80ff
Bruce <blopez6@uncc.edu>
2017-11-16 02:26:28 +0000

    username and password added successfully to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,2 @@
-# app/models/user.rb
-    
-class User < ActiveRecord::Base
-
-  has_secure_password
-
+class User < ApplicationRecord
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113222411) do
+ActiveRecord::Schema.define(version: 20171116022540) do
 
   create_table "games", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20171113222411) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "password"
   end
 
 end
